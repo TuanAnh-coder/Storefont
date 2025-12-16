1ecb05e4ce5b8b7d63bf6acc99361ea56c3f46b9
TuanAnh-coder <dqtanh.1123.cv@gmail.com>
2025-12-16 17:16:16 +0700

    delete kv_namespace

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,5 +4,5 @@ import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incre
 
 export default defineCloudflareConfig({
   incrementalCache: staticAssetsIncrementalCache,
-  enableCacheInterception: false
+  enableCacheInterception: false,
 });
