dc4b88597403bfbbcb6ef5fe136af15f7654b242
TuanAnh-coder <dqtanh.1123.cv@gmail.com>
2025-12-16 15:08:20 +0700

    change lib/config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,12 @@
-import { 
-  instantMeiliSearch,
-} from "@meilisearch/instant-meilisearch"
-export const { searchClient } = instantMeiliSearch(
-  process.env.NEXT_PUBLIC_MEILISEARCH_HOST || "",
-  process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY || ""
-)
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch"
+
+export function getSearchClient() {
+  const host = process.env.NEXT_PUBLIC_MEILISEARCH_HOST
+  const apiKey = process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY
+
+  if (!host || !apiKey) {
+    return null
+  }
+
+  return instantMeiliSearch(host, apiKey).searchClient
+}
